f379ac9ad8e63acead107f22236d882b0835513a
lamfete <lamfete@yahoo.com>
2016-12-28 16:59:51 +0700

    add angular material design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,30 +2,36 @@
 
     'use strict';
 
-    angular
-        .module('todoApp', ['ui.router', 'satellizer'])
-        .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    var app = angular.module('todoApp', ['ui.router', 'satellizer', 'ngMaterial']);
 
-            // satellizer configuration that specifies which API
-            // route the JWT should be retrieved from
-            $authProvider.loginUrl = '/api/authenticate';
+    app.config(function($stateProvider, $urlRouterProvider, $authProvider, $mdThemingProvider) {
 
-            // redirect to the auth state if any other states
-            // are requested other than users
-            $urlRouterProvider.otherwise('/auth');
+        // satellizer configuration that specifies which API
+        // route the JWT should be retrieved from
+        $authProvider.loginUrl = '/api/authenticate';
 
-            $stateProvider
-            .state('auth', {
-                url: '/auth',
-                templateUrl: '../views/authView.html',
-                controller: 'authController as auth'
-            })
-            .state('todo', {
-                url: '/todo',
-                templateUrl: '../views/todoView.html',
-                controller: 'todoController as todo'
-            });
+        // redirect to the auth state if any other states
+        // are requested other than users
+        $urlRouterProvider.otherwise('/auth');
+
+        $stateProvider
+        .state('auth', {
+            url: '/auth',
+            templateUrl: '../views/authView.html',
+            controller: 'authController as auth'
+        })
+        .state('todo', {
+            url: '/todo',
+            templateUrl: '../views/todoView.html',
+            controller: 'todoController as todo'
         });
+    });
+
+    app.controller('appCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+        $scope.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+    }]);
 })();
 
 /*
--- resources/views/index.php
+++ resources/views/index.php
@@ -1,14 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html ng-app='todoApp'>
     <head>
-        <meta charset="utf-8">
+        <!--<meta charset="utf-8"  name="viewport" content="width=device-width, initial-scale=1">-->
         <title>Todo Board</title>
-        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+        <!--<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">-->
+
+        <!-- Angular Material style sheet -->
+        <link rel="stylesheet" href="/node_modules/angular-material/angular-material.css">
+        <meta name="viewport" content="initial-scale=1" />
     </head>
-    <body ng-app='todoApp'>
-        <div class="container">
-            <div ui-view></div>
+    <body layout="column" ng-controller="appCtrl">
+        <md-toolbar layout="row">
+            <div class="md-toolbar-tools">
+                <md-button ng-click="toggleSidenav('left')" hide-gt-sm class="md-icon-button">
+                    <md-icon aria-label="Menu" md-svg-icon="css/material-design-icons/navigation/svg/production/ic_menu_48px.svg" class="avatar"></md-icon>
+                </md-button>
+                <h1>Todo Board</h1>
+            </div>
+        </md-toolbar>
+
+        <div layout="row" flex>
+            <md-sidenav layout="column" class="md-sidenav-left md-whiteframe-4dp" md-component-id="left" md-is-locked-open="$mdMedia('gt-sm')">
+                <ul>
+                    <li>
+                        <md-button>
+                            <md-icon md-svg-src="node_modules/material-design-icons/action/svg/production/ic_android_48px.svg" class="avatar"></md-icon>
+                            Riki Sholiardi
+                        </md-button>
+                    </li>
+                    <li>
+                        <md-button>
+                            <md-icon md-svg-src="css/material-design-icons/action/svg/production/ic_pets_48px.svg" class="avatar"></md-icon>
+                            Udin Khouiruddin
+                        </md-button>
+                    </li>
+                </ul>
+            </md-sidenav>
+
+            <md-content layout="column" flex class="md-padding">
+                <div ui-view></div>
+                content
+            </md-content>
         </div>
+
     </body>
 
     <!-- Application dependencies -->
@@ -20,4 +54,11 @@
     <script src="scripts/app.js"></script>
     <script src="scripts/authController.js"></script>
     <script src="scripts/todoController.js"></script>
+
+    <!-- Angular Material requires Angular.js Libraries -->
+    <script src="node_modules/angular-animate/angular-animate.js"></script>
+    <script src="node_modules/angular-aria/angular-aria.js"></script>
+
+    <!-- Angular Material Library -->
+    <script src="node_modules/angular-material/angular-material.js"></script>
 </html>
